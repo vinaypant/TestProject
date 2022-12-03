bdfb9c5cd5591167abbcbab83badb45cb23d8f17
Vinay Pant <vinay.pant@taazaa.com>
2022-12-03 07:29:56 -0500

    [master] new component added

--- TestProject/src/app/app.module.ts
+++ TestProject/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
